28bb402a5eb15f6b147fd7b2887f203c65bce850
brit-starks <brittneytechiethomas@gmail.com>
2019-05-22 21:58:08 -0700

    Added HeaderContent

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = props => {
+	return (
+		<div  className="headerContent">
+			<h1>Lambda School <span className="info">@LambdaSchool 26 jan</span> </h1>
+			<p>Let's Learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
+    </div>
+	);
+}
+
+export default HeaderContent;
