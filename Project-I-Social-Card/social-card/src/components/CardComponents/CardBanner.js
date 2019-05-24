603644ea8ec909c4b7c26cfae88779d517a85d71
brit-starks <brittneytechiethomas@gmail.com>
2019-05-23 21:31:35 -0700

    Minor updates

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import './Card.css';
 
+const CardBanner = props => {
+	return (
+		<div className="cardBanner">
+			<img src={require("../images/calculator-component.png")} id="mid-img"/>
+		</div>
+	);
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 const CardContainer = props => {
 	return (
 		<div className="cardContainer">
+			<CardBanner />
 			<CardContent />
 		</div>
 	);
