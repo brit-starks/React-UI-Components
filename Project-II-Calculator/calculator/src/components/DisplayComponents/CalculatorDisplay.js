85fc06556fbb65f58b18591295c8a2fa314b5d3d
brit-starks <brittneytechiethomas@gmail.com>
2019-05-28 20:26:26 -0700

    Updated

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -6,28 +6,31 @@ import ActionButton from '../ButtonComponents/ActionButton';
 const CalculatorDisplay = props => {
   return (
     <>
-    <div className="numButtons">
-      <div className="numRowOne">
-        <NumberButton number="1"/>
-        <NumberButton number="2" />
-        <NumberButton number="3" />
+    <div className="buttons">
+      <div className="numButtons">
+        <div className="numRowOne">
+          <NumberButton number="1"/>
+          <NumberButton number="2" />
+          <NumberButton number="3" />
+        </div>
         <div className="numRowTwo">
-        <NumberButton number="4" />
-        <NumberButton number="5" />
-        <NumberButton number="6" />
+          <NumberButton number="4" />
+          <NumberButton number="5" />
+          <NumberButton number="6" />
+        </div>
         <div className="numRowThree">
-        <NumberButton number="7" />
-        <NumberButton number="8" />
-        <NumberButton number="9" />
+          <NumberButton number="7" />
+          <NumberButton number="8" />
+          <NumberButton number="9" />
+        </div>  
       </div>
+
       <div className="calcAction">
         <ActionButton action="/" />
         <ActionButton action="x" />
         <ActionButton action="-" />
         <ActionButton action="+" />
         <ActionButton action="=" />
-        </div>
-        </div>
       </div>
     </div>
     </>
